ac4c2506a8f6617c5ccfba22b2cfdd608e4645bf
oscarbaltasar <oscarbaltasar@hotmail.com>
2023-01-20 17:53:24 +0100

    Ultimo cambio para la entrega

--- mesh.h
+++ mesh.h
@@ -19,7 +19,7 @@ class Mesh{
 
 public:
 //atributos
-	int tamTela = 10;
+	int tamTela = 50;
 	std::vector<vertex_t>* vertexList;
 	vertexFisico_t* vertexFisica = new vertexFisico_t[tamTela*tamTela];
 	std::vector<int>* faceList;
